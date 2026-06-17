ce7648a4b62e70bc62bc68e444db8231d5c7677e
LuisRuch <luisruch35@gmail.com>
2026-06-17 16:37:38 +0200

    Modbus

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        //creates ModbusClass + start Modbus
+        ModbusClass modbus = new ModbusClass();
+            modbus.connect();
+            Thread.sleep(200);
+            modbus.readAllInputs();
+
+
+        //creates CentralLogic Class + passes references
+        LogicClass central = new LogicClass(modbus);
+            central.startPollingModbus();
+            central.startPollingRest();
     }
 }
